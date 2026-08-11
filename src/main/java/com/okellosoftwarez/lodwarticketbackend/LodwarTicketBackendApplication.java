1392348f3618ed086137108fa39093342e0c2a2b
okelloEnos <enosokello@gmail.com>
2026-08-11 15:16:40 +0300

    chore: project set up

---
+++ src/main/java/com/okellosoftwarez/lodwarticketbackend/LodwarTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.okellosoftwarez.lodwarticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodwarTicketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LodwarTicketBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okellosoftwarez/lodwarticketbackend/LodwarTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okellosoftwarez.lodwarticketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodwarTicketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
